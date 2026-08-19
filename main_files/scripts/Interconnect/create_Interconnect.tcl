3c2512d7e1c8ab91bfda8a1d8d3ae86e93319131
deniz-linux <dnzgns1516@gmail.com>
2026-08-19 14:24:26 +0300

    dosya hiyerarşisi düzenlendi ve doğrulama klasörü eklendi

--- main_files/scripts/Interconnect/create_Interconnect.tcl
+++
@@ -1,6 +0,0 @@
-create_project -force Interconnect_Module Interconnect_Module -part xc7a35tcpg236-1
-
-add_files ./main_codes/rtl/desgin_sources/Interconnect/Interconnect/ -fileset sources_1
-
-update_compile_order -fileset sources_1
-update_compile_order -fileset sim_1
