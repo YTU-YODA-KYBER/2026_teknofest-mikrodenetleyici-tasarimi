776c0b6366edd6672db5f6986d3abad16a1ea463
deniz-linux <dnzgns1516@gmail.com>
2026-08-19 19:59:23 +0300

    a

--- FPGA/main_files/main_codes/rtl/desgin_sources/CPU/cv32e40p_rtl/cv32e40p_clock_gate.sv
+++
@@ -1,6 +0,0 @@
-module cv32e40p_clock_gate (
-    input  logic clk_i, en_i, scan_cg_en_i,
-    output logic clk_o
-);
-  assign clk_o = clk_i;  // FPGA'da clock gating yerine enable logic kullan
-endmodule
